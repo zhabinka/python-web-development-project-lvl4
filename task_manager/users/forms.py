651a0fb80aa660f82a9b31e0705fbc0a5a6d5794
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-07-17 12:53:48 +0300

    add user validate

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from task_manager.users.models import User
 
 class AddUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name')
+
+    def clean_first_name(self):
+        name = self.cleaned_data['first_name']
+        if len(name) > 10:
+            raise ValidationError('Длина не должна превышать 10 символов')
+
+        return name
