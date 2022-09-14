031195018d70a80f6f5cc4d69b4cb211b0b42699
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-09-14 15:51:48 +0300

    add user login

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
