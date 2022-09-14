b485dd0e5d351ba5b0190492c619b2c5391cbdd3
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-09-14 22:07:47 +0300

    add user sign up

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from task_manager import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
     path('', include('django.contrib.auth.urls')),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
